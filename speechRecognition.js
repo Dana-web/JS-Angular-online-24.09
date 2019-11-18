0239ce3506ec07ecd3b5139c28264ddc2d7d6013
Dana-web <55852277+Dana-web@users.noreply.github.com>
2019-11-18 23:44:35 +0200

    HW13

---
+++ speechRecognition.js
@@ -0,0 +1,34 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+function recognition() {
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  // recognition.lang = 'en-US';
+
+  let p = document.createElement('p');
+  const words = document.querySelector('.toText');
+  words.appendChild(p);
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+
+    p.textContent = transcript;
+
+    if (e.results[0].isFinal) {
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+
+
+  });
+
+
+  recognition.addEventListener('end', recognition.start);
+  recognition.start();
+}
+
+let letsTalk = document.querySelector('.letsTalk');
+letsTalk.addEventListener('click', recognition);
